60fe8d5e98617cf8118c72395eee2a310c4099b1
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-05-16 17:36:38 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -26,25 +26,26 @@ public class Exercise2 {
 
 	public boolean esSubconjuntoSumaExt(int a[], int suma) {
 
-		if(suma == 0){
+		//System.out.print("Evaluando ");
+		if(suma == 0){ //Si la suma es cero es porque si hay una combinacion
 			return true;
 		}
-		if( suma< 0){
+		if( suma< 0){ // Si la suma es negativa ya no vale la pena seguir
 			return false;
 		}
-		if(a.length== 1){
+		if(a.length== 1){//Si lo hay uno este debe ser igual a la suma restante
 			return a[0] == suma;
 		}
 
 		int[] n= new int[a.length-1];
-		for(int i= 1; i< a.length; i++){
+		for(int i= 1; i< a.length; i++){ //Se crea un arreglo quitando el primer elemento
 			//System.out.print(a[i-1] + "  ");
 			n[i-1]= a[i];
 		}
 
 		//System.out.print(a[a.length-1]+ "   Suma: " + suma + "\n");
-		if(a[0] % 7 == 0){
-			if(a[1]== 1)
+		if(a[0] % 7 == 0){ //Si es multiplo de 7 debe tenerse en cuenta
+			if(a[1]== 1) //Excepto si el siguiente es 1 en ese caso se debe obviar
 				return esSubconjuntoSumaExt(n,suma);
 			else
 				return esSubconjuntoSumaExt(n,suma - a[0]);
