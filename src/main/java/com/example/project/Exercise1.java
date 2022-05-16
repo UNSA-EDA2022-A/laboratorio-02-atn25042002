ba005372246243b9bc4b4154dd7f43a851765f0d
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-05-15 19:37:49 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,7 +15,16 @@ public class Exercise1 {
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
-
-		return false;
+		if(numero< 0)
+			return false;
+		if(numero== 1 && numero == 0)
+			return true;
+		else if (numero % 2==0)
+			if(numero % 4 ==0)
+				return esCuadradoPerfecto(numero/4);
+			else
+				return false;
+		else
+			return esCuadradoPerfecto((numero + 1) * (numero - 1) + 1);
 	}
 }
